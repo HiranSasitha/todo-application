e6c8978bb9eed40d44e0b078434a52d433805109
HiranSasitha <hiransasitha123@gamil.com>
2024-03-31 02:03:43 +0530

    feat(api): get todo item(byid)

--- Controllers/TodoItemController.cs
+++ Controllers/TodoItemController.cs
@@ -37,33 +37,69 @@ namespace Todo_Application.Controllers
         [Route("get-all-available-todo-items")]
         public async Task<IActionResult> GetAllTodo()       // get All available-todo-items
         {
-            
+            try
+            {
                 var todo = await _context.TodoItems.Where(todo => todo.IsAvailable).ToListAsync();
                 return Ok(todo);
-            
-           
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
         }
 
         [HttpGet]
         [Route("get-all-available-todo-items-orderby-title")]
         public async Task<IActionResult> GetAllTodoOrderByTitel()       // get All available-todo-items-orderByTitle
         {
-
-            var todo = await _context.TodoItems.Where(todo => todo.IsAvailable).OrderBy(todo => todo.Title).ToListAsync();
-            return Ok(todo);
-
+            try
+            {
+                var todo = await _context.TodoItems.Where(todo => todo.IsAvailable).OrderBy(todo => todo.Title).ToListAsync();
+                return Ok(todo);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
 
         }
 
         [HttpGet]
         [Route("get-all-unavailable-todo-items")]
-        public async Task<IActionResult> GetAllTodoUnAvailable()       // get All available-todo-items
+        public async Task<IActionResult> GetAllTodoUnAvailable()       // get All unavailable-todo-items
         {
+            try
+            {
 
-            var todo = await _context.TodoItems.Where(todo => todo.IsAvailable==false).ToListAsync();
-            return Ok(todo);
-
+                var todo = await _context.TodoItems.Where(todo => todo.IsAvailable == false).ToListAsync();
+                return Ok(todo);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
 
         }
+
+        [HttpGet]
+        [Route("get-todo-item-by-id/{id}")]
+
+        public async Task<IActionResult> GetTodoById(int id)       // get -todo-items-by-id
+        {
+            try
+            {
+
+                var todo = await _context.TodoItems.FirstOrDefaultAsync(todo => todo.Id==id);
+                return Ok(todo);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+
     }
 }
